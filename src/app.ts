1ba8a50349016f3c0c5304a4f663810cca820d81
Arya Wibowo Jati <aryajati29@gmail.com>
2024-10-01 00:42:49 +0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
