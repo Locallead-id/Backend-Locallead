02402cd4b4a8610f52d6df4281388e8373f45e72
Arya Wibowo Jati <aryajati29@gmail.com>
2024-10-20 11:08:30 +0700

    feat: implement login and register for regular user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,23 +1,37 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import { compareHashedPassword } from "../helpers/bcrypt";
 import { signToken } from "../helpers/jwt";
+import prisma from "../database/prisma";
+import { AuthRequest } from "../types/types";
 
 export class UserController {
-  static async register(req: Request, res: Response, next: NextFunction) {
+  static async register(req: AuthRequest, res: Response, next: NextFunction) {
     try {
       const { email, password, name } = req.body;
 
       if (!email) throw { name: "EmailRequired" };
       if (!password) throw { name: "PasswordRequired" };
 
-      // const createdUser = await
+      const createdUser = await prisma.user.create({
+        data: {
+          email,
+          password,
+          role: "USER",
+          profile: {
+            create: {
+              fullName: name,
+            },
+          },
+        },
+      });
+
       res.status(201).json({ id: createdUser.id, email, name });
     } catch (err) {
       next(err);
     }
   }
 
-  static async template(req: Request, res: Response, next: NextFunction) {
+  static async login(req: AuthRequest, res: Response, next: NextFunction) {
     try {
       const { email, password } = req.body;
 
@@ -25,18 +39,25 @@ export class UserController {
       if (!password) throw { name: "PasswordRequired" };
 
       // Find One User Data
-
-      if (!compareHashedPassword(password, user.password)) {
+      const foundUser = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (!foundUser) {
         throw { name: "Unauthorized" };
       }
-      const access_token = signToken({ id: user.id });
+      if (!compareHashedPassword(password, foundUser.password)) {
+        throw { name: "Unauthorized" };
+      }
+      const access_token = signToken({ id: String(foundUser.id) });
       res.status(200).json({ access_token });
     } catch (err) {
       next(err);
     }
   }
 
-  // static async template(req: Request, res: Response, next: NextFunction) {
+  // static async template(req: AuthRequest, res: Response, next: NextFunction) {
   //   try {
 
   //   } catch (err) {
