3b0e5a2ebef8e309113acc63a8626b9d8c39db1c
Arya Wibowo Jati <aryajati29@gmail.com>
2024-10-21 16:10:42 +0700

    feat: add more controllers function for users

--- src/controllers/profile.ts
+++ src/controllers/profile.ts
@@ -6,13 +6,13 @@ import prisma from "../database/prisma";
 export class ProfileController {
   static async updateProfile(req: AuthRequest, res: Response, next: NextFunction) {
     try {
-      const { address, dateOfBirth } = req.body;
+      const { address, dateOfBirth, fullName, joinDate } = req.body;
       const { user } = req;
       const updatedProfile = await prisma.profile.update({
         where: {
           id: user?.id,
         },
-        data: { address, dateOfBirth },
+        data: { address, dateOfBirth, fullName, joinDate },
       });
 
       // implement firebase image for image storing
