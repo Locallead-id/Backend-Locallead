5a4ca014304e0cbb62b277ccde6deeb745b7b3b0
Arya Wibowo Jati <aryajati29@gmail.com>
2025-01-11 13:10:34 +0700

    feat: add email validatoin helpers using regex

---
+++ src/helpers/emailValidator.ts
@@ -0,0 +1,4 @@
+export const emailValidator = (email: string): boolean => {
+  const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return regex.test(email);
+};
